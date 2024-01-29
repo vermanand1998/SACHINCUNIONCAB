b31c114e4181d4a7c862511977d3c63448995b6c
vermanand1998 <vermanand1998@gmail.com>
2024-01-29 14:45:45 +0530

    E commit

--- src/components/Helmet/Helmet.js
+++ src/components/Helmet/Helmet.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Helmet = (props) => {
-  document.title = "UNION CAB - " + props.title;
+  document.title = "UNION CABS - " + props.title;
   return <div className="w-100">{props.children}</div>;
 };
 
